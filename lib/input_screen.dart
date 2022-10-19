48a5cd39c1838ab12376679bc3bb401bcf47ae16
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-19 10:30:15 +0200

    2. Refactor Flutter Widgets (How to?)

--- lib/input_screen.dart
+++ lib/input_screen.dart
@@ -12,10 +12,47 @@ class InputScreenState extends State<InputScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('BMI CALCULATOR')),
-      body: const Center(child: Text('Body Text')),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                ReusableCard(),
+                ReusableCard(),
+              ],
+            ),
+          ),
+          const ReusableCard(),
+          Expanded(
+            child: Row(
+              children: const [
+                ReusableCard(),
+                ReusableCard(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  const ReusableCard({
+    Key? key,
+    this.color = const Color(0xFF1D1E33),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+        ),
       ),
     );
   }
